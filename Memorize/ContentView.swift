57bc17cc5dcb3bdbd26a0040ffa1c27e4422419d
Hikmet <uygurhikmet@gmail.com>
2022-04-16 17:06:14 +0300

    Start to build memorize app - from Stanford University - CS193p

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            Rectangle().fill(Color.red)
-                .ignoresSafeArea()
-                .navigationTitle("Memorize").foregroundColor(.white)
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Memorize")
         }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
